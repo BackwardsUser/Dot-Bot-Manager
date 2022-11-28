b9e15b961d416eaf4448164725a136adee3c442a
Backwards <49242785+BackwardsUser@users.noreply.github.com>
2022-11-28 10:36:18 -0500

    Changed Console Log

--- src/common/app.js
+++ src/common/app.js
@@ -6,7 +6,7 @@ var settings = require('./settings')
 
 function _init(ready) {
     var platform = os.platform().toString();
-    console.log(platform)
+    console.log(`Client is on ${platform}.`)
     if ( !(platform == 'win64' || platform == 'win32') ) return console.error("This application is developed for windows. Please download a " + os.platform + " version from the github.");
     if (!settings.hardware_acceleration) app.disableHardwareAcceleration(), console.log("Disabling Hardware Acceleration.");
     app.whenReady().then( () => {
